5fa990602f63998c77ce357994936d686400fa9e
BusK <kmabusha2@gmail.com>
2016-03-30 00:15:36 +0200

    3.1 overloading greater than, 3.2 overloading two add functions, 3.3 Testing

--- Question3.cpp
+++ Question3.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name:Kabo Onesmo Mabusha
+// Student No: 21576451
+// Date: 2016/03/29
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
@@ -22,6 +22,44 @@ private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction(int n, int d) : num(n), denom(d) { };
+	friend int operator > (Fraction fract1, Fraction fract2);
 	void print() { cout << num << "/" << denom; };
+	friend Fraction add(int Wnumber, Fraction f);
+	friend Fraction add(Fraction f, int Wnumber);
 };
+
+Fraction add(int Wnumber, Fraction f) {
+	Fraction Result((Wnumber * f.denom) + f.num, f.denom);
+	return Result;
+}
+Fraction add(Fraction f, int Wnumber) {
+	return add(Wnumber, f);
+}
+//A function that cross multiplies to create common denominator then compares numerator
+int operator > (Fraction fract1, Fraction fract2) {
+	int num1, num2;
+	num1 = fract1.num * fract2.denom;
+	num2 = fract2.num * fract1.denom;
+	if (num1 > num2)
+		return 1;
+	else
+	return 0;
+}
+
+int main() {
+	Fraction fract1(8, 10);
+	Fraction fract2(3, 4);
+	if (fract1 > fract2)
+		cout << "It is greater" << endl;
+	cout << endl;
+	cout << endl;
+	Fraction Result = add(1, fract1);
+	Result.print();
+	cout << endl;
+	cout << endl;
+	Result = add(fract2, 1);
+	Result.print();
+	cout << endl;
+	cout << endl;
+}
